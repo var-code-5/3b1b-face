b1c242a6c433d0b529068add5f6c9af61ca30b9f
var-code-5 <varshithisgod@gmail.com>
2025-12-20 01:31:14 +0530

    feat: add login page

--- src/renderer/src/components/Landing.tsx
+++ src/renderer/src/components/Landing.tsx
@@ -31,25 +31,29 @@ export default function DemoOne() {
   return (
     <div className="min-h-svh w-screen bg-linear-to-br from-[#000] to-[#1A2428] text-white flex flex-col items-center justify-center p-8">
       <div className="w-full max-w-6xl space-y-12 relative z-10">
-        <div className="flex flex-col items-center text-center space-y-8">
+        <div className="flex flex-col items-center text-center h-[30vh] space-y-4">
           <Badge variant="secondary" className="backdrop-blur-sm bg-white/10 border border-white/20 text-white hover:bg-white/20 px-4 py-2 rounded-full">
-            ✨ Next Generation Tools
+            Agentic Web-Automation
           </Badge>
           
-          <div className="space-y-6 flex items-center justify-center flex-col ">
+          <div className="space-y-6 flex items-center justify-center flex-col">
             <h1 className=" text-3xl md:text-6xl font-semibold tracking-tight max-w-3xl">
-              Discover minimalism and power in one place
+              Control what matters Automate the rest
             </h1>
             <p className="text-lg text-neutral-300 max-w-2xl">
-              Designed with aesthetics and performance in mind. Experience ultra-fast processing, advanced security, and intuitive design.
+              Built with deterministic control at its core, enabling you to govern what matters while automation handles the rest with speed and accuracy.
             </p>
-            <div className="flex flex-col sm:flex-row gap-4 items-center">
-              <Button className="text-sm px-8 py-3 rounded-xl bg-white text-black border border-white/10 shadow-none hover:bg-white/90 transition-none">
+            <div className="flex flex-col sm:flex-row gap-4 items-center ">
+              <a href="/login" className="cursor-pointer">
+              <Button className="text-sm px-8 py-3 rounded-xl bg-white text-black border border-white/10 shadow-none hover:bg-white/90 transition-none min-w-36">
                 Get Started
               </Button>
-              <Button className="text-sm px-8 py-3 rounded-xl bg-transparent text-white border border-white/20 shadow-none hover:bg-white/10 transition-none">
-                Learn More
+              </a>
+              <a href="/signup" className="cursor-pointer">
+              <Button className="text-sm px-8 py-3 rounded-xl bg-transparent text-white border border-white/20 shadow-none hover:bg-white/10 transition-none min-w-36">
+                Login
               </Button>
+              </a>
             </div>
           </div>
         </div>
